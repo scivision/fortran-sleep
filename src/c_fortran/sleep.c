81a7f07cb12292b87ab6a247315dbd2cdc893655
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-10 11:09:54 -0400

    use nanosleep

--- src/c_fortran/sleep.c
+++ src/c_fortran/sleep.c
@@ -14,7 +14,7 @@ void c_sleep(int* milliseconds){
 #else
 
 #include <stdlib.h>
-#include <unistd.h>
+#include <time.h>
 #include <errno.h>
 // https://linux.die.net/man/3/usleep
 void c_sleep(int* milliseconds)
@@ -25,7 +25,14 @@ void c_sleep(int* milliseconds)
     exit(EINVAL);
   }
 
-  int ierr = usleep(*milliseconds * 1000);
+  //int ierr = usleep(*milliseconds * 1000);
+
+  struct timespec t, r;
+
+  t.tv_sec = *milliseconds / 1000;
+  t.tv_nsec = (*milliseconds % 1000) * 1000000;
+
+  int ierr = nanosleep(&t, &r);
 
   if (ierr != 0){
     if (errno == EINTR){
@@ -34,6 +41,10 @@ void c_sleep(int* milliseconds)
     else if(errno == EINVAL){
       fprintf(stderr, "usleep bad milliseconds value\n");
       exit(EINVAL);
+    }
+      else if(errno == EFAULT){
+      fprintf(stderr, "usleep bad milliseconds value\n");
+      exit(EFAULT);
     }
     else{
       fprintf(stderr, "usleep error\n");
